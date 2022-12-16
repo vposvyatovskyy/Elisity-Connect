1a396fbe46e22a1bbc5cb151519625e895056416
vposvyatovskyy <vposvyatovskyy@future-processing.com>
2022-12-16 15:23:51 +0200

    Main commit

---
+++ Elisity Connect/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Elisity_Connect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Elisity Connect/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Elisity_Connect.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public DateTime CurrentDateTime { get; set; } = new DateTime();
+        public string ActiveTime { get; set; }
+        public MainWindowViewModel()
+        {
+            CurrentDateTime = DateTime.Now;
+            ActiveTime = string.Format("Active Time: {0}", DateTime.Now.TimeOfDay.TotalHours.ToString());
+        }
+
+    }
+}
